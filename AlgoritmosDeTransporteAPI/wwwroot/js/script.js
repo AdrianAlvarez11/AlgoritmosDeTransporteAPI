2b609ab9c240910a312d5329da4af58955e5abdb
Ramiro0318 <ramiro.val03@gmail.com>
2026-06-28 20:40:03 -0600

    Ajustes. Preparacion para la colocacion de elementos en la segunda tabla.

--- AlgoritmosDeTransporteAPI/wwwroot/js/script.js
+++ AlgoritmosDeTransporteAPI/wwwroot/js/script.js
@@ -62,16 +62,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     /////////////////////////////////
+    //////////////////////////////////////////////
+    //////////////////////////////////
+    //Primer tabla
     //Creacion de celdas
 
     const slcFilas = document.getElementById("filas");
     const slcColumnas = document.getElementById("columnas");
     const btnCrear = document.getElementById("crearTabla");
     const tablaIngreso = document.getElementById("tabla_ingreso");
-
+    const spnError = document.getElementById("error");
 
     
     btnCrear.addEventListener('click', function(e){
+        spnError.textContent = "";
         //template de encabezado y celda
         const encabezado = document.getElementById("template_header_inicial");
         const celda = document.getElementById("template_celda_input");
@@ -79,7 +83,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const filas = Number(slcFilas.value);
         const columnas = Number(slcColumnas.value);
-    
+        
+        if (filas < 2 || columnas < 2) {
+            spnError.textContent = "Indica un valor de fila y columna mayor a 1";
+            return;
+        }
+
         tablaIngreso.innerHTML = '';
 
 
@@ -146,4 +155,96 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // tablaIngreso.appendChild(crearHeader(""));
     });
+
+
+    const btnSolPaso = document.getElementById("SolucionPaso");
+
+    btnSolPaso.addEventListener('click', function(e){ 
+
+        //////////////////////////////////
+        //tabla procedimiento
+         
+        spnError.textContent = "";
+        //template de encabezado y celda
+        const encabezado = document.getElementById("template_header_inicial");
+        const celda = document.getElementById("template_celda_input");
+         
+         
+        const filas = Number(slcFilas.value);
+        const columnas = Number(slcColumnas.value);
+        
+        tablaIngreso.innerHTML = '';
+         
+         
+        const columnasTotalesGrid = columnas + 2; 
+        tablaIngreso.style.gridTemplateColumns = `repeat(${columnasTotalesGrid}, 1fr)`;
+         
+         
+        const tplHeader = document.getElementById("template_header_inicial");
+        const tplCelda = document.getElementById("template_celda_input");
+         
+        // Función para clonar y colocar texto en encabezados
+        function crearHeader(texto) {
+            const clon = tplHeader.content.cloneNode(true);
+            clon.querySelector('.texto-header').textContent = texto;
+            return clon;
+        }
+            
+        //Crear toda la primera fila
+        // Celda esquina superior izquierda
+        tablaIngreso.appendChild(crearHeader("Origen / Destino"));
+            
+        for (let c = 1; c <= columnas; c++) {
+            tablaIngreso.appendChild(crearHeader(c));
+        }
+        
+        tablaIngreso.appendChild(crearHeader("Oferta"));
+            
+            
+        // Crear las filas internas
+        
+        for (let f = 0; f < filas; f++) {
+            
+            // A. Colocar el texto de la celda de la 
+            const letraFila = String.fromCharCode(65 + f); 
+            tablaIngreso.appendChild(crearHeader(letraFila));
+            
+            // B. Celdas intermedias: Los inputs correspondientes a esta fila
+            for (let c = 0; c < columnas; c++) {
+                const clonCelda = tplCelda.content.cloneNode(true);
+                const input = clonCelda.querySelector('.input-celda');
+                
+                input.dataset.fila = letraFila;
+                input.dataset.columna = c + 1;
+                
+                tablaIngreso.appendChild(clonCelda);
+            }
+            
+            // C. Última celda de la fila: El input o espacio de la "Oferta"
+            // Puedes usar el de celda con input para que digiten la oferta
+            const clonOferta = tplCelda.content.cloneNode(true);
+            tablaIngreso.appendChild(clonOferta);
+        }
+            
+            
+            
+        // Crear la fila final
+        tablaIngreso.appendChild(crearHeader("Demanda"));
+        
+        // Inputs para los totales de la demanda (uno por cada columna numérica)
+        for (let c = 0; c < columnas; c++) {
+            const clonDemanda = tplCelda.content.cloneNode(true);
+            tablaIngreso.appendChild(clonDemanda);
+        }
+            
+    });
+            
+            
+            
+            
+            
+            
+            
+            
+            
 });
